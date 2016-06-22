b608490fc9c75b1cd7499bf64dd09f77a2ccb941
Your Name <ccsjgs1990@gmail.com>
2016-06-22 19:30:10 -0300

    libreriadelviento

---
+++ con loyout/libreriadelviento/routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  	res.render('index', { title: 'Libreria del Viento' });
+});
+
+router.get('/libros/buscar', function(req, res, next) {
+ 	res.render('books/search', { title: 'Buscar' });
+});
+
+router.get('/libros/coleccion', function(req, res, next) {
+	res.render('books/collection', { title: 'Buscar' });
+});
+
+module.exports = router;
