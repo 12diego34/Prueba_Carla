ce72734f1663fb65b8468a06ca7306d9e30df15c
Your Name <ccsjgs1990@gmail.com>
2016-07-06 13:46:12 -0300

    presentar

---
+++ PRESENTAR/public/js/controlador.js
@@ -0,0 +1,89 @@
+angular.module('myApp', ["ngRoute"])
+.controller('MostrarLibrosController', MostrarLibrosController)
+.controller('ShowBookController', ShowBookController)
+.controller('SearchBookController', SearchBookController)
+.controller('RankingController',RankingController)
+.controller('SaveController',SaveController)
+
+function MostrarLibrosController($scope, $http, $routeParams) {
+    $http.get('/l/all')
+        .success(function(data) {
+            $scope.libros = data;
+            console.log(data)
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    $scope.delete = function(id) {
+        $http.delete('/l/delete/' + id)
+            .success(function(data) {
+                $scope.libros = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error:' + data);
+            });
+    };
+}
+
+function SaveController($scope, $http) {
+    $scope.guardar =function(id){
+        $scope.pos = id;
+        $http.post('/l/new/' + $scope.pos).success(function(data) {
+            $scope.libros = data;
+        }, function(){
+                console.log('error');
+        });
+    };
+}    
+function RankingController($scope, $http) {
+    $scope.rate = function (type, id) {
+        if(type === 'positive'){
+            $scope.pos += 1;
+            $http.post('/up/' + id).success(function(data) {
+               $scope.libros = data;
+                console.log(data);
+            }, function(){
+                console.log('error');
+            });
+        }else{
+            $scope.neg -= 1;
+            $http.post('/down/' + id).success(function(data) {
+               $scope.libros = data;
+                console.log(data);
+            }, function(){
+                console.log('error');
+            });       
+        }
+    }
+} 
+
+function ShowBookController($scope, $http, $routeParams) {
+    $scope.showBook = function(){
+        var id = this.resultado.id;
+        $http.get('show/'+id)
+            .then(function(result){
+                var book = result.data.book;
+                var $title = $('#modal-info-libro .modal-title .titulo');
+            }, function(error){
+                console.log("error!", error.responseText);
+            });
+    }
+}
+
+function SearchBookController($scope, $http, $routeParams) {
+    $scope.resultados = {};
+    $scope.term = "";
+    $scope.sendForm = function () {
+        $scope.buscando = true;
+        $http.get('search/' + $scope.term)
+            .then(function(result) {
+                $scope.buscando = false;
+                $scope.resultados = result.data.resultados;
+            }, function(){
+                $scope.buscando = false;
+                console.log('SearchBookController: hubo un error');
+            });
+    };
+}
